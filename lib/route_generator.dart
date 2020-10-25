4366fa0e9de036f792a5efb207d23b197bf455b8
Adrián Soto <adrian@MacBook-Pro-de-Adrian.local>
2020-10-24 20:16:45 -0500

    Login screen, just view no functionality

---
+++ lib/route_generator.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:dext/login.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Getting arguments passed in while calling Navigator.pushNamed
+    final args = settings.arguments;
+    print(args);
+    switch (settings.name) {
+      case '/login':
+        return MaterialPageRoute(builder: (_) => LoginPage());
+      default:
+        // If there is no such named route in the switch statement, e.g. /third
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
